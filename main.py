f4e233a0ebd1dc793c0a8541929ef46c73017342
juliannowak <julian.nowak@deutschebahn.com>
2022-09-05 11:53:51 +0200

    Initialisierung vom Fenster --> Grundgerüst steht

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pygame
-import unicorn, item, enemy
 
 Screen_WIDTH = 800
 Screen_HEIGHT = 600
 
+#Initialisierung
 pygame.init()
-#Initialisierung Screen / Fenster
 screen = pygame.display.set_mode( (Screen_HEIGHT, Screen_WIDTH))
+pygame.display.set_caption("Pink Fluffy Spaceinvaders, dancing on Spaceships")
 #Hintergrund
 #TODO Mehrphaser Hintergrund, bis jetzt nur 1
-backImage = pygame.image.load()
+backImage = pygame.image.load('background/corona_lf.png').convert()
+camera_pos = [0, 0]
 #Player
 #Enemy
 #Items
@@ -18,7 +19,13 @@ backImage = pygame.image.load()
 running = True
 
 while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
+    print ("test")
+
+    screen.blit(backImage, (0, 0))
     pygame.display.update()
 
 pygame.quit()
