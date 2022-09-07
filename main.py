0466d0410bf57991182c1ec78702ceb96a233a64
juliannowak <julian.nowak@deutschebahn.com>
2022-09-07 10:50:37 +0200

    Enemy

--- main.py
+++ main.py
@@ -1,43 +1,37 @@
-#-------------------------------------------------------------------------------
-# Name:        main
-# Author:      jnowak
-# Created:     07.09.2022
-# Copyright:   (c) jnowak 2022
-# Licence:     <your licence>
-#-------------------------------------------------------------------------------
+import pygame
+import enemy
 
-import pygame, unicorn
-
+clock = pygame.time.Clock()
 Screen_WIDTH = 800
 Screen_HEIGHT = 600
 
 #Initialisierung
 pygame.init()
-screen = pygame.display.set_mode( (Screen_HEIGHT, Screen_WIDTH))
+screen1 = pygame.display.set_mode((Screen_WIDTH, Screen_HEIGHT))
 pygame.display.set_caption("Pink Fluffy Spaceinvaders, dancing on Spaceships")
 #Hintergrund
-#TODO Mehrphaser Hintergrund, bis jetzt nur 1
-backImage = pygame.image.load('background/corona_lf.png').convert()
+backImage = pygame.transform.scale(pygame.image.load('background/corona_lf.png'), (Screen_WIDTH, Screen_HEIGHT))
+
 camera_pos = [0, 0]
-#Unicorn
-unicorn1 = unicorn.Unicorn(bg_width = 10, screen_width = 10);
-unicorn_group = pygame.sprite.GroupSingle()
-unicorn_group.add(unicorn1)
+#Player
 #Enemy
+enemy1 = enemy.Enemy(200, 100)
+enemy_group = pygame.sprite.GroupSingle
+enemy_group.add(enemy1)
 #Items
 #Laser
 
 running = True
 
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-##    print ("test")
-
-    screen.blit(backImage, (0, 0))
-    unicorn_group.draw(screen)
+    screen1.blit(backImage, (0, 0))
+    enemy_group.draw(screen1)
+    #enemy_group.draw(screen)
     pygame.display.update()
 
 pygame.quit()
