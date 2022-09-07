050ce3115d4b8491b8a9f8a87667e7accd9b7071
juliannowak <julian.nowak@deutschebahn.com>
2022-09-07 10:33:05 +0200

    kaputter code stand

--- enemy.py
+++ enemy.py
@@ -1,1 +1,12 @@
 import pygame
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/aliensprite.png')
+        self.image = pygame.transform.scale2x(img)
+        self.enemyX = x
+        self.enemyY = y
+        self.rect = (100,100)
+
+    #def createEnemy(self, x,y):
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import pygame
+import enemy
 
+clock = pygame.time.Clock()
 Screen_WIDTH = 800
 Screen_HEIGHT = 600
 
 #Initialisierung
 pygame.init()
-screen = pygame.display.set_mode( (Screen_HEIGHT, Screen_WIDTH))
+screen1 = pygame.display.set_mode((Screen_WIDTH, Screen_HEIGHT))
 pygame.display.set_caption("Pink Fluffy Spaceinvaders, dancing on Spaceships")
 #Hintergrund
-#TODO Mehrphaser Hintergrund, bis jetzt nur 1
-backImage = pygame.image.load('background/corona_lf.png').convert()
+backImage = pygame.transform.scale(pygame.image.load('background/corona_lf.png'), (Screen_WIDTH, Screen_HEIGHT))
+
 camera_pos = [0, 0]
 #Player
 #Enemy
+enemy1 = enemy.Enemy(200, 100)
+enemy_group = pygame.sprite.GroupSingle
+enemy_group.add(enemy1)
 #Items
 #Laser
 
 running = True
 
 while running:
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    print ("test")
-
-    screen.blit(backImage, (0, 0))
+    screen1.blit(backImage, (0, 0))
+    enemy_group.draw(screen1)
+    #enemy_group.draw(screen)
     pygame.display.update()
 
 pygame.quit()
