7bf7f01f5ea99757c9eea1b4903e9c7137963e9f
NeshJulian <112848305+NeshJulian@users.noreply.github.com>
2022-09-08 09:57:08 +0200

    Add files via upload
    Spaceinvaders Unterricht

--- main.py
+++ main.py
@@ -1,148 +1,118 @@
-import random
-
 import pygame
-import enemy
-import item
-import laser
-import map
-import unicorn
+import random
+import pygame.mixer
 
-clock = pygame.time.Clock()
-Screen_WIDTH = 1000
-Screen_HEIGHT = 750
-
-#Initialisierung
 pygame.init()
-screen = pygame.display.set_mode((Screen_WIDTH, Screen_HEIGHT))
-pygame.display.set_caption("Pink Fluffy Spaceinvaders, dancing on Spaceships")
-level_counter = 2
-#Farben
-white = (0, 0, 0)
-pink = (255, 0, 255)
-#Fonts
-font = pygame.font.SysFont("Arial", 60)
-game_end = font.render("Spiel gewonnen", True, pink)
-game_over = font.render("Game Over", True, pink)
-next_level = font.render("Level 2", True, pink)
+#Screen initialisierung
+screen = pygame.display.set_mode( (800,600) )
 #Hintergrund
-backImage1 = map.Map('background/Hintergrund1.png')
-backImage2 = map.Map('background/Hintergrund2.png')
-map_group1 = pygame.sprite.GroupSingle(backImage1)
-map_group2 = pygame.sprite.GroupSingle(backImage2)
+bgimage = pygame.image.load('.idea/img/Background/starBackground.png').convert()
 #Player
-playerX = 60
-playerY = random.randint(100, 750)
-unicorn1 = unicorn.Unicorn(playerX, playerY)
-unicorn_group = pygame.sprite.Group(unicorn1)
-zaehler = 0
-#Enemy
-enemyX = 970
-enemyY = 30
-enemy_group = pygame.sprite.Group()
-gameGoOn = True
-enemy_counter = 0
-newEnemys = True
-
-for i in range(5):
-    for j in range(10):
-        enemy_group.add(enemy.Enemy(enemyX, enemyY))
-        enemyY += 75
-        enemy_counter += 1
-    enemyX -= 100
-    enemyY = 30
-
-#Items
-itemX = random.randint(300, 500)
-itemY = random.randint(50, 700)
-item_group = pygame.sprite.GroupSingle(item.Item(itemX, itemY))
-itemUsed = True
+player = pygame.image.load('.idea/img/player.png').convert_alpha()
+playerPosx = 350
+playerPosy = 500
+playerDeltax = 0
 #Laser
-laserX = -50
-laserY = -50
-laser1 = laser.Laser(laserX, laserY)
-laser_group = pygame.sprite.GroupSingle(laser1)
-isShooting = True
+laserimg = pygame.image.load('.idea/img/laserRed.png').convert_alpha()
+isShooting = False
+laserPosx = -100
+laserPosy = 0
+laserDeltay = -10
+trefferAnzahl = 0
+laser_sound = pygame.mixer.Sound('.idea/snd/laser3.ogg')
+#Enemy
+enemy = pygame.image.load('.idea/img/enemyShip.png').convert_alpha()
+enemyPosx = random.randint(50, 550)
+enemyPosy = 50
+enemyDeltax = 50
+enemyDeltay = 50
+enemyCounter = random.randint(50,150)
+explosion_sound = pygame.mixer.Sound('.idea/snd/explosion5.ogg')
+#Schriftart
+trefferfont = pygame.font.SysFont('Arial', 30)
+trefferimg = trefferfont.render("Treffer: " +str(0), True, (255,255,255))
 
 running = True
+
 while running:
-    clock.tick(60)
+    #eingabe
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_w:
-                unicorn1.fly(-1)
-            if event.key == pygame.K_s:
-                unicorn1.fly(1)
+            if event.key == pygame.K_RIGHT:
+                playerDeltax += 3
+            if event.key == pygame.K_LEFT:
+                playerDeltax -= 3
+            if event.key == pygame.K_SPACE and not isShooting:
+                isShooting = True
+                laser_sound.play()
+                laserPosx = playerPosx+46
+                laserPosy = playerPosy-38
+
         if event.type == pygame.KEYUP:
-            unicorn1.fly(0)
+            playerDeltax = 0
 
-    if pygame.sprite.spritecollide(unicorn1, item_group, True):
-        laser1.boost = True
+    #update
+    #enemyPosy += enemyDeltay
+    if enemyPosy > 550:
+        enemyPosy = 550
 
-    if pygame.sprite.spritecollide(unicorn1, enemy_group, False):
-        gameGoOn = False
+    enemyPosx += enemyDeltax
+
+    if enemyPosx < 0:
+        enemyPosx = 0
+        enemyDeltax *= -1
+        enemyPosy += enemyDeltay
+
+    if enemyPosx > 702:
+        enemyPosx = 702
+        enemyDeltax *= -1
+        enemyPosy += enemyDeltay
+
+    if enemyPosx + 98 > playerPosx and enemyPosx < playerPosx + 99:
+        if enemyPosy + 50 > playerPosy and enemyPosy < playerPosy + 75:
+            enemyPosx -= enemyDeltax
+
+    playerPosx += playerDeltax
+    if playerPosx < 0:
+        playerPosx = 0
+
+    if playerPosx > 701:
+        playerPosx = 701
+
+    if isShooting:
+        laserPosy = laserPosy + laserDeltay
+
+    if enemyPosx + 98 > laserPosx and enemyPosx < laserPosx + 33:
+        if enemyPosy + 50 > laserPosy and enemyPosy < laserPosy + 9:
+            trefferAnzahl += 1
+            print("Trefferanzahl: " , trefferAnzahl)
+            trefferimg = trefferfont.render("Treffer: " +str(trefferAnzahl), True, (255,255,255))
+            enemyCounter -= 1
+            enemyPosx = random.randint(50, 550)
+            enemyPosy = 10
+            laserPosx = -100
+            isShooting = False
+            explosion_sound.play()
+
+    if laserPosy < -30:
+        print("oben")
+        laserPosx = -100
+        laserPosy = 1000
         isShooting = False
 
-    if pygame.sprite.spritecollide(laser1, enemy_group, True):
-        laser1.rect.x = -100
-        isShooting = True
-        enemy_counter -= 1
+    for i in range ((int) (800/bgimage.get_width()) +1):
+        for j in range ((int) (600/bgimage.get_height()) +1):
+            screen.blit(bgimage, (i*254, j*256))
 
-    zaehler += 1
-    item_group.update(itemUsed)
+    screen.blit(player,(playerPosx, playerPosy))
+    screen.blit(laserimg, (laserPosx, laserPosy))
+    screen.blit(enemy, (enemyPosx, enemyPosy))
+    screen.blit(trefferimg, (10,10))
 
-    if gameGoOn:
-        enemy_group.update()
-        unicorn_group.update(zaehler)
-
-    if isShooting and laser1.rect.x < -50:
-        laser1.rect.x = unicorn1.rect.x + 30
-        laser1.rect.y = unicorn1.rect.y
-        isShooting = False
-
-    if laser1.rect.x > Screen_WIDTH:
-        laser1.rect.x = unicorn1.rect.x +30
-        laser1.rect.y = unicorn1.rect.y
-
-    if not isShooting and gameGoOn:
-        laser1.movement()
-
-    if zaehler >= 20:
-        zaehler = 0
-
-    if not gameGoOn:
-        #TODO Funktioniert noch nicht
-        screen.blit(game_over, (30, 50))
-
-    if enemy_counter == 0:
-        level_counter -= 1
-        screen.blit(game_end, (10, 10))
-
-    if level_counter == 0:
-        screen.blit(game_end, (10, 10))
-
-    if level_counter ==2:
-        backImage1.update()
-        map_group1.draw(screen)
-    elif level_counter == 1 and gameGoOn:
-        if newEnemys:
-            for i in range(5):
-                for j in range(10):
-                    enemy_group.add(enemy.Enemy(enemyX, enemyY))
-                    enemyY += 75
-                    enemy_counter += 1
-            enemyX -= 100
-            enemyY = 30
-            print(enemy_counter)
-        newEnemys = False
-        backImage2.update()
-        map_group2.draw(screen)
-
-    unicorn_group.draw(screen)
-    item_group.draw(screen)
-    enemy_group.draw(screen)
-    laser_group.draw(screen)
     pygame.display.update()
 
 pygame.quit()
+
